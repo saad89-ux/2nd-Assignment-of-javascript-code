5844d43124d3f5ee166d3a0f96a07ed9c1aa722d
saad <saadkamran874@gmail.com>
2025-01-28 21:27:25 +0500

    my second practice of javascript

---
+++ app.js
@@ -0,0 +1,106 @@
+/*document.write("Hello world!");*/
+/*let userName=("");
+console.log( userNmae);*/
+ /*let myName=prompt("Enter your name");
+console.log(myName);
+
+
+let message =("Hello world!");
+alert(message);
+
+let userName=("Ali");
+alert(userName);
+
+let yourAge=prompt("Enter your age");
+alert(yourAge +   "  Prevent this page from creating additional dialogs  ");
+
+
+let uniName=prompt("Enter your uni name");
+alert(uniName);
+
+
+let yourModule=prompt("Enter your module name");
+alert(yourModule +   " <br/> Prevent this page from creating additional dialogs "); */
+/*let favFood = "PIZZA\n Pizz\n PIZ\n PI\n P";
+alert(favFood);
+let yourFood=("PIZZA   <br/>   Pizz  <br/>   PIZ  <br/>  PI  <br/>  P  <br/>");
+document.write(yourFood); //it only works on HTml related documents in short on Dom; */
+
+
+
+/*let childrenNumber=prompt("Enter your number of childers");
+let wifeName=prompt("Enter your wife name");
+let geographicalLocation=prompt("Enter your geographical location");
+let jobTitle=prompt("Enter your job title");
+alert( "you wil become" + " " + jobTitle  + " " + "in" + " " + geographicalLocation + " " + "and married to " + " " + wifeName + " " + " with 2 kids"  ); */
+
+/*let emailAddress=prompt("Enter your email address");
+alert( "My email address is" + " " + emailAddress); */
+
+/*let book = "A Smarterway to learn A javascript";
+alert( "I am trying lo learn from the book" + "  "+ book);*/
+
+
+/*let myName=prompt("Enter your name");
+let yourUniName=prompt("Enter your uniname");
+let yourAge=prompt("Enter your age");
+let yourProfession=prompt("Enter your Profession");
+
+alert( "MY name is" + " " +myName  + " my age is " + " " + yourAge + " my uni name is" + " " + yourUniName  + " my profession is " + yourProfession );*/
+
+/*document.write("yah ! I can  write html through Javascript...........!");*/
+
+/*let age= prompt("Enter your age");
+console.log("I am"+ "  " + age + " " + " years old");*/
+
+/*let visited = prompt("Enter your visisted times on this websites");
+alert(" you have visisted" + "  " + visited + "th" + " " + "Times on this website");*/
+
+/*let birthdate = prompt(" Enter your birth date");
+document.write("MY bithday date is"+ " " + birthdate + "<br/> Data type of my declared variable is number");*/
+
+
+/*let visitorName=prompt("Enter your name");
+let productTitle=prompt("Enter your product name");
+let productQuantity=prompt("Enter your product quantity");
+document.write(visitorName + " " + " ordered" +  " " +productQuantity + " " + productTitle +" " + "on XYZ Clothing store");*/
+/*let product = "  Cycle" ,   yourName="Saad" ,   favGame= " Cricket";
+console.log(favGame + " " + yourName + " " + product ) ;*/
+
+                // Lets calculate your percentage
+/*let grandMarks=500;
+let obtMarks=334;
+let yourPercentage=obtMarks/grandMarks*100;
+let passingMarks=grandMarks-obtMarks;
+console.log(grandMarks, obtMarks,yourPercentage,passingMarks);*/
+
+ //lets calcu;ate modulus of three
+ /*let num1=41;
+console.log(num1**3 % 2);*/
+ //increment and decrement operator
+/*let num2=34;
+let num3=64;
+console.log(num2++ , num3++);
+console.log(num2++ , num3++);
+console.log(num2++ , num3++);*/
+
+// lets calculate table of number
+/*tableNumber=2;
+count=1;
+console.log(tableNumber + " " + " x" + " " + count + " =" + " " + count++ *tableNumber);
+console.log(tableNumber + " " + " x" + " " + count + " =" + " " + count++ *tableNumber);
+console.log(tableNumber + " " + " x" + " " + count + " =" + " " + count++ *tableNumber);
+console.log(tableNumber + " " + " x" + " " + count + " =" + " " + count++ *tableNumber);
+console.log(tableNumber + " " + " x" + " " + count + " =" + " " + count++ *tableNumber);
+console.log(tableNumber + " " + " x" + " " + count + " =" + " " + count++ *tableNumber);
+console.log(tableNumber + " " + " x" + " " + count + " =" + " " + count++ *tableNumber);
+console.log(tableNumber + " " + " x" + " " + count + " =" + " " + count++ *tableNumber);
+console.log(tableNumber + " " + " x" + " " + count + " =" + " " + count++ *tableNumber);
+console.log(tableNumber + " " + " x" + " " + count + " =" + " " + count++ *tableNumber);
+console.log(tableNumber + " " + " x" + " " + count + " =" + " " + count++ *tableNumber);
+console.log(tableNumber + " " + " x" + " " + count + " =" + " " + count++ *tableNumber); */
+
+
+
+
+
